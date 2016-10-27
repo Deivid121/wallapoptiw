c9018707103f4884bcd8467328458336edce4808
Oshernan <o.racso11@gmail.com>
2016-10-27 20:24:15 +0000

    Filtro para la sesion

---
+++ src/main/java/es/uc3m/tiw/filtrers/FilterLogin.java
@@ -0,0 +1,60 @@
+package es.uc3m.tiw.filtrers;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class FilterLogin
+ */
+@WebFilter("/*")
+public class FilterLogin implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FilterLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpSession sesion =  ((HttpServletRequest)request).getSession(); 
+		//buscamos el token de autenticacion
+		if (sesion.getAttribute("autenticado")!=null &&(boolean)sesion.getAttribute("logeado")==true) { 
+			
+			chain.doFilter(request, response); 
+		}else{
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+		}
+		chain.doFilter(request, response);
+	}
+		
+	
+
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
