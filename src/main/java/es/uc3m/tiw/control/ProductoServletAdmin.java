570eda7e0dcb03915485666b4603c6f52e2baaf0
Oshernan <o.racso11@gmail.com>
2016-10-28 07:28:36 +0000

    Cambios en la navegacion

--- src/main/java/es/uc3m/tiw/control/ProductoServletAdmin.java
+++ src/main/java/es/uc3m/tiw/control/ProductoServletAdmin.java
@@ -1,17 +1,22 @@
 package es.uc3m.tiw.control;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
@@ -20,42 +25,42 @@ import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 
 import es.uc3m.tiw.wallapop.dominios.Producto;
+import es.uc3m.tiw.wallapop.dominios.Usuario;
 import es.uc3m.tiw.wallapoptiw.daos.ProductoDAO;
 import es.uc3m.tiw.wallapoptiw.daos.ProductoDAOImpl;
+import es.uc3m.tiw.wallapoptiw.daos.UsuarioDAO;
+import es.uc3m.tiw.wallapoptiw.daos.UsuarioDAOImpl;
 
 /**
- * Servlet implementation class eliminarProductoClave
+ * Servlet implementation class usuarioServlet
  */
-@WebServlet("/eliminarProductoClave")
-public class eliminarProductoClave extends HttpServlet {
+@WebServlet("/productoServletAdmin")
+public class ProductoServletAdmin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Producto producto;
-	private ProductoDAO pdao;
-	private String pagina;
-	private ServletConfig config;
+	private ServletConfig config; 
+	private ProductoDAO dao;
+       
 	@PersistenceContext(unitName="wallapoptiw")
     EntityManager em;
     @Resource
     UserTransaction ut;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public eliminarProductoClave() {
+    public ProductoServletAdmin() {
         super();
         // TODO Auto-generated constructor stub
     }
-    public void init(ServletConfig config) throws ServletException {
-    	this.config = config;
-   		pdao = new ProductoDAOImpl();
-   		pdao.setConexion(em);
-   		pdao.setTransaction(ut);
-    }
+    @Override
+	public void init(ServletConfig config) throws ServletException {
+		this.config = config;
+		dao = new ProductoDAOImpl();
+		dao.setConexion(em);
+		dao.setTransaction(ut);
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doPost(request,response);
 	}
 
@@ -63,17 +68,30 @@ public class eliminarProductoClave extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int clave  = Integer.parseInt(request.getParameter("id"));
-		try{
-			producto = pdao.buscarProductoClave(clave);
-			 pdao.eliminarProducto(producto);
-		}catch (SecurityException | IllegalStateException | SQLException | NotSupportedException | SystemException
-				| RollbackException | HeuristicMixedException | HeuristicRollbackException e) {
+		// TODO Auto-generated method stub
+		Producto prod;
+		HttpSession sesion = request.getSession();
+		
+		prod=(Producto) sesion.getAttribute("producto");
+		prod.setTitulo(request.getParameter("nombre"));
+		prod.setCategoria(request.getParameter("categoria"));
+		prod.setDescripcion(request.getParameter("descripcion"));
+		prod.setPrecio(Integer.parseInt(request.getParameter("precio")));
+		prod.setEstado(request.getParameter("estado"));
+		//prod.setImagen(request.getParameter("imagen"));
+		
+		
+		
+		try {
+			sesion.setAttribute("producto", dao.actualizarProducto(prod));
+			config.getServletContext().getRequestDispatcher("/AdminPanel").forward(request, response);
+		} catch (SQLException | SecurityException | IllegalStateException | RollbackException | HeuristicMixedException | HeuristicRollbackException | SystemException | NotSupportedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		config.getServletContext().getRequestDispatcher("/AdminPanel").forward(request, response);
+		
+		
+		
 	}
 
 }
